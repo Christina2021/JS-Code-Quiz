bfeb90b64f3cf1ef08281fc15cb53954a7737a5a
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-10-18 21:46:21 -0700

    added new js file for index.html page

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+//Linked to index.html
+
+// alert("We are linked");  confirmed link; remove once JS content entered
+
+//This file will be used for the start page
+
+//Once start quiz button is clicked on, the start-page section will display none and the questions section will display
